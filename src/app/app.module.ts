882b928b0c3f190f5202e401aa4013ac7c792ea2
Alvin Lim <alim459@gmail.com>
2016-11-27 16:10:02 -0800

    remove old dependency from appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,12 @@ import { HomeComponent } from './home/home.component';
 import { EventsComponent } from './events/events.component';
 import { EventDataService } from './shared/services/event-data.service';
 import { AppToggleService } from './shared/services/app-toggle.service';
-import { BannerComponent } from './banner/banner.component';
  
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     EventsComponent,
-    BannerComponent
   ],
   imports: [
     BrowserModule,
